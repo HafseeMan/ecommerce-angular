413f842f5b320fc8dbe015c88063c375104f6a12
HafseeMan <hafoosah1@gmail.com>
2021-10-07 17:08:48 +0100

    populating frontend folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,10 +7,18 @@ import {MatCardModule} from '@angular/material/card'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductDetailsPageComponent } from './components/product-details-page/product-details-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartPageComponent,
+    HomePageComponent,
+    ProductCardComponent,
+    ProductDetailsPageComponent
   ],
   imports: [
     BrowserModule,
