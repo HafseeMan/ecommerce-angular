5f978768e1343dacb78f9c04ed5b6fd1654c08bc
HafseeMan <hafoosah1@gmail.com>
2021-10-08 15:47:36 +0100

    adding restapi and json server

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CartPageComponent } from './components/cart-page/cart-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProductDetailsPageComponent } from './components/product-details-page/product-details-page.component';
 
+const routes: Routes = [
 
-const routes: Routes = [];
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'cart', component:  CartPageComponent},
+  { path: 'product-details/:id', component:  ProductDetailsPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/components/home-page/home-page.component.ts
+++ frontend/src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/shared/product-model';
+import { RestApiService } from 'src/app/shared/rest-api.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  
+  Products: any = []
 
-  constructor() { }
+  constructor(
+    public restApi: RestApiService
+  ) { }
 
   ngOnInit(): void {
+    this.displayProducts()
+  }
+
+  //Get products to display
+  displayProducts(){
+    return this.restApi.getProducts().subscribe((data: {}) => {
+      this.Products = data;
+    })
   }
 
 }
