96fa9c0c751e5acb42df14cdce7d8121d7f83699
HafseeMan <hafoosah1@gmail.com>
2021-10-22 15:52:59 +0100

    carrt updATE

--- New folder/src/app/product.ts
+++
@@ -1,8 +0,0 @@
-export class Product {
-    public id!: number;
-    public name!: string;        
-    public details!: string;
-    public price!: string;
-    public cart!: boolean;
-    public img!: string;
-}
